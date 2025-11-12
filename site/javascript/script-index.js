2da3eae237c1b652fbd8d402c8c5b1b3e8aadfb1
Hendrik Kaiber <hendrik.b.kaiber@gmail.com>
2025-11-12 08:30:47 -0300

    Adicionar botão de login

--- site/javascript/script-index.js
+++ site/javascript/script-index.js
@@ -3,14 +3,17 @@
 // Get the modal
 let modal_sala = document.getElementById("cadastro_sala");
 let modal_usuario = document.getElementById("cadastro_usuario");
+let modal_login = document.getElementById("modal_login");
 
 // Get the button that opens the modal
 let btn_sala = document.getElementById("botao_cadastrar_sala");
 let btn_usuario = document.getElementById("botao_cadastrar_usuario");
+let btn_login = document.getElementById("botao_janela_login");
 
 // Get the <span> element that closes the modal
 let span_sala = document.getElementsByClassName("close")[0];
 let span_usuario = document.getElementsByClassName("close")[1];
+let span_login = document.getElementsByClassName("close")[2];
 
 // When the user clicks on the button, open the modal
 btn_sala.onclick = function() {
@@ -19,6 +22,10 @@ btn_sala.onclick = function() {
 
 btn_usuario.onclick = function() {
   modal_usuario.style.display = "block";
+}
+
+btn_login.onclick = function() {
+  modal_login.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -30,10 +37,15 @@ span_usuario.onclick = function() {
   modal.style.display = "none";
 }
 
+span_login.onclick = function() {
+  modal.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target === modal_sala || event.target === modal_usuario) {
+  if (event.target === modal_sala || event.target === modal_usuario || event.target === modal_login) {
     modal_sala.style.display = "none";
     modal_usuario.style.display = "none";
+    modal_login.style.display = "none";
   }
 } 
